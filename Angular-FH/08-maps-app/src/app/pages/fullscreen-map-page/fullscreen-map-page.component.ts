d7026d5b0fd4b02d94724c35bd8a8573ebeef40b
MXTR327 <maxramirez327@gmail.com>
2025-07-26 19:08:43 -0500

    Tema: Integrar Mapbox GL y mejorar las funcionalidades de los mapas
    - Se añadió Mapbox GL como dependencia para la representación de mapas.
    - Se implementó una página de mapa a pantalla completa con controles de zoom y visualización de coordenadas.
    - Se creó un componente de minimapa para mostrar la ubicación de las casas.
    - Se mejoró la página de casas para mostrar tarjetas de casas con minimapas.
    - Se desarrolló la página de marcadores para gestionar los marcadores del mapa con funciones de clic y eliminación.
    - Se actualizó el componente de la barra de navegación para eliminar el botón no utilizado.
    - Se modificó index.html para incluir el CSS de Mapbox y establecer el tema de Halloween.
    - Se refactorizaron los estilos de los componentes de mapa y minimapa a pantalla completa.

--- Angular-FH/08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ Angular-FH/08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,83 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
+import mapboxgl from "mapbox-gl";
+import { environment } from '../../../environments/environment';
+import { DecimalPipe, JsonPipe } from '@angular/common';
+import { MarkersPageComponent } from "../markers-page/markers-page.component";
 
-@Component({
+mapboxgl.accessToken = environment.mapboxKey;
+
+@Component( {
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe, JsonPipe, MarkersPageComponent],
   templateUrl: './fullscreen-map-page.component.html',
-})
-export class FullscreenMapPageComponent { }
+  styleUrl: "./fullscreen-map-page.styles.css",
+} )
+export class FullscreenMapPageComponent implements AfterViewInit
+{
+  divElement = viewChild<ElementRef>( "map" );
+  map = signal<mapboxgl.Map | null>( null );
+
+  zoom = signal( 14 );
+  coordinates = signal(
+    {
+      lng: -74.5,
+      lat: 40,
+    }
+  );
+
+  zoomEffect = effect( () =>
+  {
+    if ( !this.map() ) return;
+
+    this.map()?.setZoom( this.zoom() );
+    // this.map()?.zoomTo( this.zoom() );
+  } );
+
+  async ngAfterViewInit()
+  {
+    if ( !this.divElement()?.nativeElement ) return;
+
+    await new Promise( resolve => setTimeout( resolve, 80 ) );
+
+    const element = this.divElement()?.nativeElement;
+    const { lng, lat } = this.coordinates();
+
+
+    const map = new mapboxgl.Map(
+      {
+        container: element, // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [ lng, lat ], // starting position [lng, lat]
+        zoom: this.zoom() // starting zoom
+      }
+    );
+
+    this.mapListeners( map );
+  }
+
+  mapListeners( map: mapboxgl.Map )
+  {
+    map.on( "zoomend", event =>
+    {
+      const newZoom = event.target.getZoom();
+      this.zoom.set( newZoom );
+    } );
+
+    map.on( "moveend", () =>
+    {
+      const center = map.getCenter();
+      this.coordinates.set( center );
+    } );
+
+    map.on( "load", () =>
+    {
+      console.log( "Map Loaded" );
+    } );
+
+    map.addControl( new mapboxgl.FullscreenControl() );
+    map.addControl( new mapboxgl.NavigationControl() );
+    map.addControl( new mapboxgl.ScaleControl() );
+
+    this.map.set( map );
+  }
+}
